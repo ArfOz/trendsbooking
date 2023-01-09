a0247eefc6c71cb2d732761e085577b70c264109
serhatkoyuncu <serhat_koyuncu@icloud.com>
2023-01-09 09:05:15 +0300

    components created

---
+++ apps/frontend-customer/src/pages/MainPages/Shop/ShopSection/ShopRight/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+const StyledCardSwiper1 = styled.div`
+    color: pink;
+`;
+export function CardSwiper1(props) {
+    return (
+        <StyledCardSwiper1>
+            <h1>Welcome to CardSwiper1!</h1>
+        </StyledCardSwiper1>
+    );
+}
+export default CardSwiper1;
