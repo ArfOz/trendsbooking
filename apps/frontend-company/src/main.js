d162cd9259afb3f397b2a64adfcee7d05542e701
serhatkoyuncu <serhat_koyuncu@icloud.com>
2023-01-20 22:29:10 +0300

    theme js added company

--- apps/frontend-company/src/main.js
+++ apps/frontend-company/src/main.js
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <App />
+        {/* <App /> */}
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+                <App />
+        </ThemeProvider>
     </StrictMode>,
 );
