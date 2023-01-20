52cb886c18564bd3a2a0729446f5be97a6ddcd4f
serhatkoyuncu <serhat_koyuncu@icloud.com>
2023-01-20 22:49:29 +0300

    main.js fix

--- apps/frontend-company/src/main.js
+++ apps/frontend-company/src/main.js
@@ -1,15 +1,9 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-<<<<<<< HEAD
 import {ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import theme from "./theme";
-=======
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
->>>>>>> d162cd9259afb3f397b2a64adfcee7d05542e701
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,11 +11,7 @@ root.render(
         {/* <App /> */}
         <ThemeProvider theme={theme}>
             <CssBaseline />
-<<<<<<< HEAD
             <App />
-=======
-                <App />
->>>>>>> d162cd9259afb3f397b2a64adfcee7d05542e701
         </ThemeProvider>
     </StrictMode>,
 );
