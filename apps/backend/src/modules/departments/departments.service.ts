d0d32c8b71100fdf1f94a271ee84c780d4533a8b
Arif Trends <arif.ozturk@trendsbooking.com>
2023-02-10 22:09:55 +0100

    update

--- apps/backend/src/modules/departments/departments.service.ts
+++ apps/backend/src/modules/departments/departments.service.ts
@@ -1,4 +1,45 @@
+import { Prisma } from '@prisma/client';
+import { BadRequestExceptionType } from './../../../../../libs/shared/src/enums/exception.type';
+import { BadRequestException } from './../../../../../libs/shared/src/exceptions/bad-request.exception';
+import ResponseMessage from '@shared/enums/response-message.json';
+import { AddDepartmentsJsonDto } from './dtos/departments.dto';
 import { Injectable } from '@nestjs/common';
+import { DepartmentService } from '@database';
 
 @Injectable()
-export class DepartmentsService {}
+export class DepartmentsService {
+
+    constructor(
+        private readonly departmentService: DepartmentService,
+    ){
+
+    }
+    async add(user, input: AddDepartmentsJsonDto) {
+        console.log(user, input);
+
+        if (!input.Salon || !input.ServiceType || !input.Workers) {
+            throw new BadRequestException(
+                BadRequestExceptionType.BAD_REQUEST,
+                new Error(ResponseMessage.TR426),
+                426,
+            );
+        }
+
+        const data :Prisma.DepartmentCreateInput= {
+            Workers: input.Workers?,
+            Salon: input.Salon,
+            // ServiceType?: DepartmentCreateServiceTypeInput | Enumerable<ServiceType>
+            // CompanyUser?: CompanyUserCreateNestedOneWithoutDepartmentsInput
+        }
+
+        await this.departmentService.create({
+
+        })
+
+
+        return {
+            Data: ResponseMessage.TR204,
+            Success: true,
+        };
+    }
+}
