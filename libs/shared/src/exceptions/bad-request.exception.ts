52056ee0d1853aac6fa4cd35df3e6c1e58ad93d3
Arif Trends <arif.ozturk@trendsbooking.com>
2023-02-15 14:41:16 +0100

    update error messages

--- libs/shared/src/exceptions/bad-request.exception.ts
+++ libs/shared/src/exceptions/bad-request.exception.ts
@@ -8,10 +8,9 @@ export class BadRequestException extends TrendsException {
         type: keyof typeof BadRequestExceptionType,
         // error?: Error,
         details: Error,
-        
         code: number,
     ) {
-        const status= HttpStatus.BAD_REQUEST
+        const status = HttpStatus.BAD_REQUEST;
         super(type, details, status, code);
     }
 }
