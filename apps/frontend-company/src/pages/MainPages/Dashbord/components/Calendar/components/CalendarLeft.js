76659ff8098bb0f11b80c6b6593ddf5084e8ff0d
SenFaruk <kiowa297@gmail.com>
2023-03-03 19:16:57 +0300

    calender component added

--- apps/frontend-company/src/pages/MainPages/Dashbord/components/Calendar/components/CalendarLeft.js
+++ apps/frontend-company/src/pages/MainPages/Dashbord/components/Calendar/components/CalendarLeft.js
@@ -1,29 +1,39 @@
-
-import React from 'react';
+import * as React from 'react';
+import dayjs from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import { Box } from '@mui/material';
 
 
-function CalendarLeft() {
-   
+
+export default function CalendarLeft() {
+    const [value, setValue] = React.useState(dayjs('2022-04-07'));
 
     return (
         <Box
             sx={{
                 width: '48%',
                 height: '100%',
-                borderRadius: '10px',
                 background: '#FFFFFF',
+                borderRadius: '10px',
                 display: 'flex',
-                alignItems: 'center',
                 justifyContent: 'center',
-               
-          
-
+                alignItems: 'center',
             }}
         >
-           <h1>CALENDAR</h1>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <StaticDatePicker
+                    orientation="landscape"
+                    openTo="day"
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+            </LocalizationProvider>
         </Box>
     );
 }
-
-export default CalendarLeft;
