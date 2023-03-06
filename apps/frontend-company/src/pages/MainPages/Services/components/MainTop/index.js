8ae833bead9a4983346ffbda2ebb140311b7aeb7
tahiraydins <tahiraydin06@gmail.com>
2023-03-06 15:51:02 +0300

    hizmetler navbar eklendi

---
+++ apps/frontend-company/src/pages/MainPages/Services/components/MainTop/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from '@mui/material/Box';
+import { Typography } from '@mui/material';
+
+function MainTop() {
+    return <Box>
+      <Typography>Tahir</Typography>
+    </Box>;
+}
+
+export default MainTop;
---
+++ apps/frontend-company/src/pages/MainPages/Services/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import NavbarLeft from './components/NavbarLeft';
+import NavbarRight from './components/NavbarRight';
+import { AppBar, Toolbar } from '@mui/material';
+import NavbarTop from './components/NavbarTop';
+
+function Navbar() {
+    return (
+        <AppBar
+            position="sticky"
+            color="info"
+            sx={{
+                boxShadow: 'none',
+            }}
+        >
+            <NavbarTop />
+            <Toolbar
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    ml: 5,
+                    mr: 5,
+                }}
+            >
+                <NavbarLeft />
+                <NavbarRight />
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
