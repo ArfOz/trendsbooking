76fc9bb04f8694b2938ac77421c2d1f6a6922424
SenFaruk <kiowa297@gmail.com>
2023-03-13 21:48:32 +0300

    randevuLeft checkBox finished

--- apps/frontend-company/src/pages/MainPages/Randevu/components/RandevuLeft/components/CheckBox.js
+++ apps/frontend-company/src/pages/MainPages/Randevu/components/RandevuLeft/components/CheckBox.js
@@ -1,31 +1,44 @@
 import React, { useState } from 'react';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import line from '../../../../../../assets/line.png';
 import {
     Typography,
     Button,
     Checkbox,
     FormControlLabel,
     Box,
+    Divider,
 } from '@mui/material';
 
 const CheckBox = () => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+    const [options, setOptions] = useState([
+        { id: 1, label: 'Hepsi', isChecked: false },
+        { id: 2, label: 'Acun', isChecked: false },
+        { id: 3, label: 'Hadise', isChecked: false },
+        { id: 3, label: 'Murat', isChecked: false },
+        { id: 3, label: 'Beyaz', isChecked: false },
+    ]);
 
-    const onClickEvent = () => {
+    const handleDropdownClick = () => {
         setIsOpen(!isOpen);
     };
 
-    const [checked, setChecked] = useState(false);
-
-    const handleChange = (event) => {
-        setChecked(event.target.checked);
+    const handleCheckboxClick = (id) => {
+        const updatedOptions = options.map((option) =>
+            option.id === id
+                ? { ...option, isChecked: !option.isChecked }
+                : option,
+        );
+        setOptions(updatedOptions);
     };
+
     return (
-        <>
+        <div>
             <Button
                 variant="contained"
-                onClick={onClickEvent}
+                onClick={handleDropdownClick}
                 sx={{
                     backgroundColor: '#F2F8FF',
                     borderRadius: '5px',
@@ -53,38 +66,45 @@ const CheckBox = () => {
                     Personel
                 </Typography>
             </Button>
-           
-            <Box
-                sx={{
-                    ml: 10,
-                }}
-            >
-                <input type="checkbox" name="hepsi" id="" />
-                <label for="hepsi"> Hepsi</label>
-                <br></br>
-            </Box>
-            <Box
-                sx={{
-                    ml: 12,
-                }}
-            >
-                <input type="checkbox" name="acun" id="" />
-                <label for="acun"> Acun</label>
-                <br></br>
-                <input type="checkbox" name="tarık" id="" />
-                <label for="tarık"> Tarık</label>
-                <br></br>
-                <input type="checkbox" name="derya" id="" />
-                <label for="derya"> Derya</label>
-                <br></br>
-                <input type="checkbox" name="acun" id="" />
-                <label for="acun"> Acun</label>
-                <br></br>
-                <input type="checkbox" name="tarık" id="" />
-                <label for="tarık"> Tarık</label>
-                <br></br>
-            </Box>
-        </>
+
+            {isOpen && (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: '70%',
+                            borderLeft: '1px solid #D9D9D9',
+                        }}
+                    >
+                        {options.map((option) => (
+                            <Box
+                                sx={{
+                                    ml: 2,
+                                }}
+                                key={option.id}
+                            >
+                                <input
+                                    type="checkbox"
+                                    id={option.id}
+                                    checked={option.isChecked}
+                                    onChange={() =>
+                                        handleCheckboxClick(option.id)
+                                    }
+                                />
+                                <label htmlFor={option.id}>
+                                    {option.label}
+                                </label>
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+            )}
+        </div>
     );
 };
 
