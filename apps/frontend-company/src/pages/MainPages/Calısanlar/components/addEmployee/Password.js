c561de2176e78b046bbe0c207fba818252656652
SenFaruk <kiowa297@gmail.com>
2023-04-13 22:51:36 +0300

    working page sao buttons added

--- apps/frontend-company/src/pages/MainPages/Calısanlar/components/addEmployee/Password.js
+++ apps/frontend-company/src/pages/MainPages/Calısanlar/components/addEmployee/Password.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
-import { Box, TextField } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
 function Password() {
+    const [showCheckboxes, setShowCheckboxes] = useState(false);
+    const handleButtonClick = () => {
+        setShowCheckboxes(!showCheckboxes);
+    };
     return (
         <Box
             sx={{
@@ -13,13 +19,65 @@ function Password() {
                 mt: 3,
             }}
         >
-            <TextField
-                label="Rol Ata"
-                variant="outlined"
-                sx={{
+            <Box  sx={{
                     width: '48%',
+                    background: '#FFFFFF',
+                    border: '1.36634px solid #9A9A9A',
+                    boxShadow: '0px 0px 21.8614px rgba(234, 76, 137, 0.06)',
+                    borderRadius: '8.19802px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                     <Button
+                variant="contained"
+                onClick={handleButtonClick}
+                endIcon={
+                    showCheckboxes ? (
+                        <ArrowDropDownCircleIcon color="primary" />
+                    ) : (
+                        <PlayCircleIcon color="primary" />
+                    )
+                }
+                sx={{
+                    background: '#FFFFFF',
+                    border: 'none',
+                    boxShadow: '0none',
+                    width: '100%',
+                    
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                   
+                 
+                    '&:hover': {
+                        backgroundColor: '#FFFF',
+                    },
+                   m:0.99
+                  
                 }}
-            />
+            >
+                <Typography
+                    sx={{
+                        fontFamily: 'Roboto',
+                        fontStyle: 'normal',
+                        fontWeight: 500,
+                        fontSize: '14px',
+                        lineHeight: '20px',
+                        textAlign: 'center',
+                        textTransform: 'capitalize',
+                        color: '#9A9A9A',
+                    }}
+                >
+                    Rol Ata
+                </Typography>
+            </Button>
+           
+
+            </Box>
+           
+
+
             <TextField
                 label="Şifre"
                 variant="outlined"
