ed6d6d9fce6589994e29d1307ae485a09aee1c77
SenFaruk <kiowa297@gmail.com>
2023-05-06 23:24:29 +0300

    customer home page Links is ok.....

--- apps/frontend-customer/src/components/Navbar/NavbarFirst/components/RightNavbar.js
+++ apps/frontend-customer/src/components/Navbar/NavbarFirst/components/RightNavbar.js
@@ -1,22 +1,50 @@
 import React from 'react';
 
-import { AppBar, Box, Stack, Toolbar, Typography, Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import GTranslateIcon from '@mui/icons-material/GTranslate';
 import { Link } from 'react-router-dom';
 import { style } from './style';
 
 const RightNavbar = () => {
-    const Buttons = [{ title: 'Giriş Yap', path: '/Auth/Login' }];
+    const Links = [
+        { title: 'Giriş Yap', path: '/Auth/Login' },
+        { title: 'İşletme Hesabı', path: '/Auth/Register' },
+    ];
+
     return (
         <>
             {/* right navbar */}
             <Box sx={style.navbarbuttoncontainer}>
-                <Button sx={style.navbarbuttons}>
-                    <Link to={Buttons.path}>{Buttons.title}</Link>
-                </Button>
-                <Button variant="outlined" sx={style.navbarbuttons}>
-                    İşletme Hesabı
-                </Button>
+                {Links.map((item, index) => (
+                    <Button
+                        variant="outlined"
+                        key={item.title}
+                        sx={{
+                            textTransform: 'capitalize',
+                            '&:hover': {
+                                backgroundColor: 'white',
+                                boxShadow: 'none',
+                                border: 'none',
+                            },
+                            ...(item.title === 'Giriş Yap' && {
+                                border: 'none',
+                            }),
+                            ...(item.title === 'İşletme Hesabı' && {
+                                '&:hover': {
+                                    backgroundColor: 'white',
+                                    boxShadow: 'none',
+                                },
+                            }),
+                        }}
+                    >
+                        <Link
+                            to={item.path}
+                            style={{ textDecoration: 'none', color: 'inherit' }}
+                        >
+                            {item.title}
+                        </Link>
+                    </Button>
+                ))}
                 <Button
                     variant="contained"
                     sx={style.buttons}
