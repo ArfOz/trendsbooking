f561595999b6de14f438fc099d543dd573d9fed2
SenFaruk <kiowa297@gmail.com>
2023-05-17 23:23:01 +0300

    logo link is ok..

--- apps/frontend-company/src/components/Navbar/PersonelNavbar/components/NavbarLeft.js
+++ apps/frontend-company/src/components/Navbar/PersonelNavbar/components/NavbarLeft.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Box, } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import trendbiz from '../../../../assets/trendbiz.png';
-
 
 const NavbarLeft = () => {
     return (
         <>
-            <Box>
-                <img src={trendbiz} alt="" />
-            </Box>
+            <Button>
+                <Link to="/">
+                    <img src={trendbiz} alt="" />
+                </Link>
+            </Button>
         </>
     );
 };
