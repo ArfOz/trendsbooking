a435859949a73d0d3b022339f538c58c09d424a2
Arif Trends <arif.ozturk@trendsbooking.com>
2023-05-30 00:26:35 +0200

    update filter type

--- apps/backend/src/modules/departments/departments.controller.ts
+++ apps/backend/src/modules/departments/departments.controller.ts
@@ -159,23 +159,7 @@ export class DepartmentController {
     @Post('addphotos')
     @UseInterceptors(
         FileInterceptor('file', {
-            fileFilter: (req: any, file: any, cb: any) => {
-                if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
-                    // Allow storage of file
-                    cb(null, true);
-                } else {
-                    // Reject file
-                    cb(
-                        new HttpException(
-                            `Unsupported file type ${extname(
-                                file.originalname,
-                            )}`,
-                            HttpStatus.BAD_REQUEST,
-                        ),
-                        false,
-                    );
-                }
-            },
+            fileFilter: imageFileFilter,
         }),
     )
     @ApiBody({
--- libs/shared/src/filter/image.filter.ts
+++ libs/shared/src/filter/image.filter.ts
@@ -1,6 +1,18 @@
-export const imageFileFilter = (req, file, callback) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return callback(new Error('Only image files are allowed!'), false);
+import { extname } from 'path';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export const imageFileFilter = (req: any, file: any, cb: any) => {
+    if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+        // Allow storage of file
+        cb(null, true);
+    } else {
+        // Reject file
+        cb(
+            new HttpException(
+                `Unsupported file type ${extname(file.originalname)}`,
+                HttpStatus.BAD_REQUEST,
+            ),
+            false,
+        );
     }
-    callback(null, true);
 };
